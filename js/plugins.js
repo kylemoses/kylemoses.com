f619dc71d0b319f28ec02b92b50dd808fd8bba91
Moses <kmoses@c1mn708jdty3.gateway.pace.com>
2015-05-17 16:56:15 -0400

    continuing with the overhauld of my website

--- js/plugins.js
+++ js/plugins.js
@@ -1,98 +1,120 @@
-window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
-(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
-(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
+window.log = function f() {
+	log.history = log.history || [];
+	log.history.push(arguments);
+	if (this.console) {
+		var args = arguments,
+			newarr;
+		args.callee = args.callee.caller;
+		newarr = [].slice.call(args);
+		if (typeof console.log === 'object') log.apply.call(console.log, console, newarr);
+		else console.log.apply(console, newarr);
+	}
+};
+(function(a) {
+	function b() {}
+	for (var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","), d; !!(d = c.pop());) {
+		a[d] = a[d] || b;
+	}
+})
+(function() {
+	try {
+		console.log();
+		return window.console;
+	} catch (a) {
+		return (window.console = {});
+	}
+}());
 
-$(document).ready(function(){
-
+$(document).ready(function() {
 	contactForm();
 	fancybox();
-	
+	setDateYear();
 });
 
 function contactForm() {
-
 	$("#contact_form").submit(function() {
-	    $.post('contact_form.php', {
-	    	name: $('#name').val(), 
-	    	email: $('#email').val(), 
-	    	organization: $('#organization').val(), 
-	    	message: $('#message').val(), 
-	    	contactFormSubmitted: 'yes'}, 
-	    	function(data) {
-	        	$("#formResponse").html(data).fadeIn('100');
-	        	//reset feilds
-	        	$('#name').val('Name');
-	        	$('#email').val('Email'); 
-	        	$('#organization').val('Organization'); 
-	        	$('#message').val('Message'); 
-	    	}, 'text');
-	    return false;
+		$.post('contact_form.php', {
+				name: $('#name').val(),
+				email: $('#email').val(),
+				organization: $('#organization').val(),
+				message: $('#message').val(),
+				contactFormSubmitted: 'yes'
+			},
+			function(data) {
+				$("#formResponse").html(data).fadeIn('100');
+				//reset feilds
+				$('#name').val('Name');
+				$('#email').val('Email');
+				$('#organization').val('Organization');
+				$('#message').val('Message');
+			}, 'text');
+		return false;
 	});
-	
-	$('#name').focus(function(){
+
+	$('#name').focus(function() {
 		var name = $('#name').val();
 		if (name === 'Name') {
 			$(this).val('');
 		}
 	});
-	
-	$('#name').blur(function(){
+
+	$('#name').blur(function() {
 		var name = $('#name').val();
 		if (name === '') {
 			$(this).val('Name');
 		}
 	});
 
-	$('#email').focus(function(){
+	$('#email').focus(function() {
 		var name = $('#email').val();
 		if (name === 'Email') {
 			$(this).val('');
 		}
 	});
-	
-	$('#email').blur(function(){
+
+	$('#email').blur(function() {
 		var name = $('#email').val();
 		if (name === '') {
 			$(this).val('Email');
 		}
 	});
 
-	$('#phone').focus(function(){
+	$('#phone').focus(function() {
 		var name = $('#phone').val();
 		if (name === 'Phone') {
 			$(this).val('');
 		}
 	});
-	
-	$('#phone').blur(function(){
+
+	$('#phone').blur(function() {
 		var name = $('#phone').val();
 		if (name === '') {
 			$(this).val('Phone');
 		}
 	});
 
-	$('#organization').focus(function(){
+	$('#organization').focus(function() {
 		var name = $('#organization').val();
 		if (name === 'Organization') {
 			$(this).val('');
 		}
 	});
-	
-	$('#organization').blur(function(){
+
+	$('#organization').blur(function() {
 		var name = $('#organization').val();
 		if (name === '') {
 			$(this).val('Organization');
 		}
 	});
 
-	$('#message').focus(function(){
+	$('#message').focus(function() {
 		var name = $('#message').val();
 		if (name === 'Message') {
 			$(this).val('');
 		}
 	});
-	
-	$('#message').blur(function(){
+
+	$('#message').blur(function() {
 		var name = $('#message').val();
 		if (name === '') {
 			$(this).val('Message');
@@ -103,47 +125,47 @@ function contactForm() {
 
 
 function fancybox() {
-
-
-
-
 	$("a.portfolio").click(function() {
-		$.fancybox([
-			{
-				'href'	: 'img/portfolio/drDeign_website.jpg',
-				'title'	: 'Dr. Deign Author & Speaker - <a href="http://www.drdeign.com" target="_blank">View Site</a>'
-				
+		$.fancybox([{
+				'href': 'img/portfolio/drDeign_website.jpg',
+				'title': 'Dr. Deign Author & Speaker - <a href="http://www.drdeign.com" target="_blank">View Site</a>'
+
 			},
-			
+
 			{
-				'href'	: 'img/portfolio/springfield_live.jpg',
-				'title'	: 'Springfield, OH community website - website design'
+				'href': 'img/portfolio/springfield_live.jpg',
+				'title': 'Springfield, OH community website - website design'
 			},
-			
+
 			{
-				'href'	: 'img/portfolio/wearewireless_homepage.jpg',
-				'title'	: 'We Are Wireless Verizon Wireless Retailer - website design'
+				'href': 'img/portfolio/wearewireless_homepage.jpg',
+				'title': 'We Are Wireless Verizon Wireless Retailer - website design'
 			},
-			
+
 			{
-				'href'	: 'img/portfolio/ymca_website.jpg',
-				'title'	: 'Local YMCA Branch - website design'
+				'href': 'img/portfolio/ymca_website.jpg',
+				'title': 'Local YMCA Branch - website design'
 			},
-			
+
 			{
-				'href'	: 'img/portfolio/IVH_Webiste_v2.jpg',
-				'title'	: 'Irvin House Vineyard Domestic Winery in Charleston, SC - website design'
+				'href': 'img/portfolio/IVH_Webiste_v2.jpg',
+				'title': 'Irvin House Vineyard Domestic Winery in Charleston, SC - website design'
 			},
-			
+
 			{
-				'href'	: 'img/portfolio/waveland_website_home.jpg',
-				'title'	: 'Waveland Advocacy Group - website design'
+				'href': 'img/portfolio/waveland_website_home.jpg',
+				'title': 'Waveland Advocacy Group - website design'
 			}
 
 		], {
-			'type'              : 'image'
+			'type': 'image'
 		});
 		return false;
 	});
 
+}
+
+function setDateYear() {
+	var year = new Date();
+	$('#curYear').html(year.getFullYear());
 }
