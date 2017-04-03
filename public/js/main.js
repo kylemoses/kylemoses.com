e52aaf6d41a837900a4946cbdfa966de7ab165b1
kmoses <moses.kyle@gmail.com>
2017-04-03 00:28:12 -0400

    some project reworking; adding contact form

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,151 @@
-function setDateYear() {
-	var year = new Date();
-	$('#curYear').html(year.getFullYear());
+'use strict'
+//namespacing
+var kyle = {};
+kyle.ui = {};
+// contact page form 
+kyle.ui.contactForm = function(formElm, resElm) {
+	var self = this;
+	// jquery vars
+	var $formElm = $("#" + formElm);
+	var $resElm = $("#" + resElm);
+
+	this.formData = "";
+	this.valid = false;
+	this.validationMsg = "";
+
+	this.init = function() {
+		self.events.resetForm();
+		self.events.hideResponse();
+		self.bindEvents();
+	};
+	this.bindEvents = function() {
+		$('body').on('submit', $formElm, function(e) {
+			e.preventDefault();
+			self.formData = self.events.getFormData($formElm);
+			self.events.validateFields();
+			self.events.submitForm(self.formData);
+		});
+	};
+	this.events = {
+		getFormData: function($formElm) {
+			var unindexed_array = $formElm.serializeArray();
+			var indexed_array = {};
+			$.map(unindexed_array, function(n, i) {
+				indexed_array[n['name']] = n['value'];
+			});
+			return JSON.stringify(indexed_array);
+		},
+		submitForm: function(data) {
+			if (self.validationMsg === "") {
+				$.ajax({
+					type: "POST",
+					url: "/contact",
+					data: self.formData,
+					contentType: "application/json",
+					success: function(data) {
+						self.events.displayResponse(data);
+					},
+					error: function(data) {
+						self.events.displayResponse(data.responseText, "error");
+					}
+				}).done(function() {
+					self.events.resetForm();
+				});
+			} else {
+				self.events.displayResponse(self.validationMsg, "error");
+			}
+		},
+		resetForm: function() {
+			// reset form elms
+			$('input[type=text],input[type=email]').val('').removeClass('error');
+			$('textarea').val('').removeClass('error');
+		},
+		displayResponse: function(msg, error) {
+			error === "error" ? $resElm.addClass("error") : $resElm.removeClass("error");
+			$resElm.slideDown(300).html(msg);
+		},
+		hideResponse: function() {
+			$resElm.slideUp(300).removeClass('error').html('');
+		},
+		validateFields: function() {
+			self.validationMsg = "";
+			var name = $formElm.find("#name");
+			var email = $formElm.find("#email");
+			var subject = $formElm.find("#subject");
+			var message = $formElm.find("#message");
+			// validate name
+			if (name.val().length < 3) {
+				name.addClass('error');
+				self.validationMsg += "<li>Please Include your name.</li>"
+			} else {
+				name.removeClass('error');
+			}
+			// validate email
+			if (email.val().length < 1) {
+				email.addClass('error');
+				self.validationMsg += "<li>Please include a your email address.</li>"
+			} else if (!validateEmail(email.val())) {
+				email.addClass('error');
+				self.validationMsg += "<li>Please use a valid email.</li>"
+			} else {
+				email.removeClass('error');
+			}
+
+			function validateEmail(email) {
+				var re = /\S+@\S+\.\S+/;
+				return re.test(email);
+			}
+			// validate subject
+			if (subject.val().length < 2) {
+				subject.addClass('error');
+				self.validationMsg += "<li>Please include a subject line for your message.</li>"
+			} else {
+				subject.removeClass('error');
+			}
+			// validate message
+			if (message.val().length < 5) {
+				message.addClass('error');
+				self.validationMsg += "<li>Please include a message for me.</li>"
+			} else {
+				message.removeClass('error');
+			}
+		}
+	};
+	// Public methods
+	return {
+		init: function() {
+			self.init();
+		}
+	};
+};
+// display year method
+kyle.ui.displayYear = function(elemId) {
+	var self = this;
+	var date = "";
+	// jquery vars
+	var $yearElm = $('#' + elemId);
+	// init
+	this.init = function() {
+		// store new date obj
+		date = new Date();
+		// bind events
+		self.bindEvents();
+	};
+	// bind events to handlers
+	this.bindEvents = function() {
+		$yearElm.html(self.events.getYear(date));
+	};
+	// private event methods
+	this.events = {
+		getYear: function(dateObj) {
+			// get the year from the passed in date object
+			return dateObj.getFullYear();
+		}
+	};
+	// Public methods
+	return {
+		init: function() {
+			self.init();
+		}
+	};
 }
--- server.js
+++ server.js
@@ -1,40 +1,79 @@
 // server.js
 // load the things we need
-var express = require('express');
 var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser')
+var email = require("emailjs");
+
 var app = express();
-
-var port = process.env.PORT || 8080; // set our port
-
+// set our port
+var port = process.env.PORT || 8080;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-// use res.render to load up an ejs view file
+// use things
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+// load api data
+var kyleData = "";
+http.get("http://www.kylemoses.com/api/kyle", function(res) {
+	console.log("Got response: " + res.statusCode);
+	if (res.statusCode == 200) {
+		console.log("Got value: " + res.statusMessage);
+	}
+	res.on("data", function(chunk) {
+		kyleData = JSON.parse(chunk);
+	});
+	res.on('end', function() {
+		console.log(kyleData);
+	})
+}).on('error', function(e) {
+	console.log("Got error: " + e.message);
+});
 
 // index page 
 app.get('/', function(req, res) {
-	var data = "";
-	http.get("http://www.kylemoses.com/api/kyle", function(res) {
-		console.log("Got response: " + res.statusCode);
-		if (res.statusCode == 200) {
-			console.log("Got value: " + res.statusMessage);
-		}
-		res.on("data", function(chunk) {
-			data = JSON.parse(chunk);
-		});
-		res.on('end', function() {
-			console.log(data);
-			renderfunc(data);
-		})
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
-	});
-
-	function renderfunc(data) {
-		res.render('pages/index', { data: data });
-	}
+	res.render('pages/index', { data: kyleData });
 });
 
-app.use(express.static(__dirname + '/public'));
+// contact Page
+app.get('/contact', function(req, res) {
+	res.render('pages/contact', { data: kyleData })
+});
+
+app.post('/contact', function(req, res) {
+	var formData = "";
+	// get the data from the post request
+	formData = req.body;
+	// setup the smtp server
+	var emailServer = email.server.connect({
+		user: "moses.kyle@gmail.com",
+		password: "khujatcgvhoxpfal",
+		host: "smtp.gmail.com",
+		ssl: true
+	});
+	// Compile the message body
+	emailMessage = "";
+	emailMessage += 'From: ' + formData.name + '\r\n';
+	emailMessage += 'Email: ' + formData.email + '\r\n';
+	emailMessage += 'Subject: ' + formData.subject + '\r\n';
+	emailMessage += 'Message: ' + formData.message + '\r\n';
+	// send the message and get a callback with an error or details of the message that was sent
+	emailServer.send({
+		text: emailMessage,
+		from: formData.email,
+		to: "moses.kyle@gmail.com",
+		cc: "",
+		subject: 'A message from your website!: ' + formData.subject
+	}, function(err, message) {
+		if (err) {
+			console.log(err);
+			res.status(500).send("There was a problem!");
+		} else {
+			console.log(message);
+			res.status(200).send("message sent!");
+		}
+	});
+});
 
 if (typeof(PhusionPassenger) != 'undefined') {
 	console.log('Example app listening with passenger');
